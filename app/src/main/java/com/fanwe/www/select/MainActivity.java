9fc15e30cc81592ae85f440181af631951f91181
zjun <565061763@qq.com>
2017-08-25 17:40:18 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/select/MainActivity.java
+++ app/src/main/java/com/fanwe/www/select/MainActivity.java
@@ -15,21 +15,25 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        view_tab = (CustomTabView) findViewById(R.id.view_tab);
 
+        view_tab = (CustomTabView) findViewById(R.id.view_tab);
         view_tab.getViewConfig(view_tab.tv_title)
-                .setTextColorNormal(Color.parseColor("#616161")) //未选中字体颜色
+                .setTextColorNormal(Color.parseColor("#616161")) //正常字体颜色
                 .setTextColorSelected(Color.parseColor("#f57c00")) //选中字体颜色
-                .setTextSizeNormal(40) //未选中字体大小
+
+                .setTextSizeNormal(40) //正常字体大小
                 .setTextSizeSelected(60) //选中字体大小
-                .setAlphaNormal(0.2f) //未选中透明度
+
+                .setAlphaNormal(0.2f) //正常透明度
                 .setAlphaSelected(1.0f); //选中透明度
-        // .setHeightNormal(10)  //未选中高度
+
+        // .setHeightNormal(10)  //正常高度
         // .setHeightSelected(30); //选中高度
 
         view_tab.getViewConfig(view_tab.view_underline)
                 .setBackgroundColorSelected(Color.parseColor("#f57c00")) //选中背景颜色
-                .setVisibilityNormal(View.INVISIBLE) //未选中可见状态
+
+                .setVisibilityNormal(View.INVISIBLE) //正常可见状态
                 .setVisibilitySelected(View.VISIBLE); //选中可见状态
 
         view_tab.setSelected(false); //设置初始化状态为未选中
