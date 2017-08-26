11b67b46066ccddb024b464ba4f3ae072093012b
zjun <565061763@qq.com>
2017-08-26 14:14:15 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/select/MainActivity.java
+++ app/src/main/java/com/fanwe/www/select/MainActivity.java
@@ -6,7 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import com.fanwe.library.select.view.SDSelectViewContainer;
+import com.fanwe.library.select.SDSelectViewContainer;
+
 
 public class MainActivity extends AppCompatActivity
 {
