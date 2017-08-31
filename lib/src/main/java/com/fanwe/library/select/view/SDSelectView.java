992a5961e5171a9ab851192081dd4836c12155c9
zjun <565061763@qq.com>
2017-08-31 16:30:02 +0800

    开源协议

--- lib/src/main/java/com/fanwe/library/select/view/SDSelectView.java
+++ lib/src/main/java/com/fanwe/library/select/view/SDSelectView.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 zhengjun, fanwe (http://www.fanwe.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.fanwe.library.select.view;
 
 import android.content.Context;
@@ -10,10 +25,6 @@ import android.widget.FrameLayout;
 import com.fanwe.library.select.ISDSelectViewContainer;
 import com.fanwe.library.select.SDSelectViewConfig;
 import com.fanwe.library.select.SDSelectViewContainer;
-
-/**
- * Created by Administrator on 2017/8/27.
- */
 
 public class SDSelectView extends FrameLayout implements ISDSelectViewContainer
 {
