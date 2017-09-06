f4cd02f5b557cbad96380737d0b80202e843a88f
zjun <565061763@qq.com>
2017-09-06 15:43:41 +0800

    demo测试

---
+++ app/src/main/java/com/fanwe/www/select/CustomTab.java
@@ -0,0 +1,51 @@
+package com.fanwe.www.select;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import com.fanwe.library.select.view.SDSelectView;
+
+/**
+ * Created by Administrator on 2017/9/6.
+ */
+public class CustomTab extends SDSelectView
+{
+    public CustomTab(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        init();
+    }
+
+    private TextView tv_title;
+    private View view_underline;
+
+    private void init()
+    {
+        setContentView(R.layout.view_custom_tab);
+        tv_title = (TextView) findViewById(R.id.tv_title);
+        view_underline = findViewById(R.id.view_underline);
+
+        config(tv_title) // 获得对应的config
+                .setTextColorNormal(Color.parseColor("#616161")) // 正常字体颜色
+                .setTextColorSelected(Color.parseColor("#f57c00")) // 选中字体颜色
+                .setTextSizeNormal(40) // 正常字体大小
+                .setTextSizeSelected(60) // 选中字体大小
+                .setAlphaNormal(0.2f) // 正常透明度
+                .setAlphaSelected(1.0f) // 选中透明度
+                .setSelected(false); //设置默认未选中
+
+        config(view_underline) // 获得对应的config
+                .setBackgroundColorSelected(Color.parseColor("#f57c00")) // 选中背景颜色
+                .setVisibilityNormal(View.INVISIBLE) // 正常可见状态
+                .setVisibilitySelected(View.VISIBLE) // 选中可见状态
+                .setSelected(false); //设置默认未选中
+
+        config(this) // 获得对应的config
+                .setWidthNormal(150)
+                .setWidthSelected(300)
+                .setSelected(false); //设置默认未选中
+    }
+}
--- app/src/main/java/com/fanwe/www/select/MainActivity.java
+++ app/src/main/java/com/fanwe/www/select/MainActivity.java
@@ -1,47 +1,21 @@
 package com.fanwe.www.select;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
-
-import com.fanwe.library.select.SDSelectViewContainer;
-
 
 public class MainActivity extends AppCompatActivity
 {
-    private TextView mTvTitle;
-    private View mViewUnderline;
-
-    private SDSelectViewContainer mSelectViewContainer = new SDSelectViewContainer();
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTvTitle = (TextView) findViewById(R.id.tv_title);
-        mViewUnderline = findViewById(R.id.view_underline);
-
-        mSelectViewContainer.config(mTvTitle) // 获得mTvTitle对应的config
-                .setTextColorNormal(Color.parseColor("#616161")) // 正常字体颜色
-                .setTextColorSelected(Color.parseColor("#f57c00")) // 选中字体颜色
-                .setTextSizeNormal(40) // 正常字体大小
-                .setTextSizeSelected(60) // 选中字体大小
-                .setAlphaNormal(0.2f) // 正常透明度
-                .setAlphaSelected(1.0f) // 选中透明度
-                .setSelected(false); //设置默认未选中
-
-        mSelectViewContainer.config(mViewUnderline) // 获得mViewUnderline对应的config
-                .setBackgroundColorSelected(Color.parseColor("#f57c00")) // 选中背景颜色
-                .setVisibilityNormal(View.INVISIBLE) // 正常可见状态
-                .setVisibilitySelected(View.VISIBLE) // 选中可见状态
-                .setSelected(false); //设置默认未选中
     }
 
     public void onClickBtnChangeState(View view)
     {
-        mSelectViewContainer.setSelected(!mTvTitle.isSelected()); //切换容器中所有View的选中状态
+        View view_tab = findViewById(R.id.view_tab);
+        view_tab.setSelected(!view_tab.isSelected());//切换View的选中状态
     }
 }
