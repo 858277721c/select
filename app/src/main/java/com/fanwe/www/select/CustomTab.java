7b44013c94d68476f00140b063deaf501e64edc9
zjun <565061763@qq.com>
2017-09-15 13:21:38 +0800

    重构

--- app/src/main/java/com/fanwe/www/select/CustomTab.java
+++ app/src/main/java/com/fanwe/www/select/CustomTab.java
@@ -1,7 +1,6 @@
 package com.fanwe.www.select;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
@@ -19,33 +18,13 @@ public class CustomTab extends SDSelectView
         init();
     }
 
-    private TextView tv_title;
-    private View view_underline;
+    public TextView tv_title;
+    public View view_underline;
 
     private void init()
     {
         setContentView(R.layout.view_custom_tab);
         tv_title = (TextView) findViewById(R.id.tv_title);
         view_underline = findViewById(R.id.view_underline);
-
-        configText(tv_title) // 获得对应的config，也可以在外部调用
-                .setTextColor(Color.parseColor("#616161"), Color.parseColor("#f57c00"))
-                .setTextSize(40, 60)
-                .setAlpha(0.2f, 1.0f)
-                .setSelected(false); //设置默认未选中
-
-        config(view_underline) // 获得对应的config，也可以在外部调用
-                .setBackgroundColor(null, Color.parseColor("#f57c00"))
-                .setVisibility(View.INVISIBLE, View.VISIBLE)
-                .setSelected(false); //设置默认未选中
-
-        config(this) // 获得对应的config，也可以在外部调用
-                .setWidth(150, 300)
-                .setSelected(false); //设置默认未选中
-
-        config(this).setHeight(100, 100)
-                .setHeight(null, null);
-
-        config(this).setAlpha(1f, 1f);
     }
 }
--- app/src/main/java/com/fanwe/www/select/MainActivity.java
+++ app/src/main/java/com/fanwe/www/select/MainActivity.java
@@ -1,21 +1,40 @@
 package com.fanwe.www.select;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
+    private CustomTab mCustomTab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCustomTab = (CustomTab) findViewById(R.id.view_tab);
+
+        mCustomTab.configText(mCustomTab.tv_title) // 获得对应的config
+                .setTextColor(Color.parseColor("#616161"), Color.parseColor("#f57c00")) //正常和选中颜色
+                .setTextSize(40, 60) //正常和选中字体大小
+                .setAlpha(0.2f, 1.0f) //正常和选中透明度
+                .setSelected(false); //设置默认未选中
+
+        mCustomTab.config(mCustomTab.view_underline) // 获得对应的config
+                .setBackgroundColor(null, Color.parseColor("#f57c00")) //正常和选中背景
+                .setVisibility(View.INVISIBLE, View.VISIBLE) //正常和选中可见状态
+                .setSelected(false); //设置默认未选中
+
+        mCustomTab.config(mCustomTab) // 获得对应的config
+                .setWidth(150, 300) //正常和选中宽度
+                .setHeight(300, null) //正常和选中高度，null表示不处理该状态
+                .setSelected(false); //设置默认未选中
     }
 
     public void onClickBtnChangeState(View view)
     {
-        View view_tab = findViewById(R.id.view_tab);
-        view_tab.setSelected(!view_tab.isSelected());//切换View的选中状态
+        mCustomTab.setSelected(!mCustomTab.isSelected());//切换View的选中状态
     }
 }
